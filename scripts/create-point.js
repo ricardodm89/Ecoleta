82abb0bbb1a5135c0a75a8aa403df6ae4e556c5f
ricardodm89 <ricado89@gmail.com>
2020-06-09 16:45:48 -0300

    Populate Cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -15,8 +15,30 @@ function populateUFs(){
 
 populateUFs()
 
+function getcities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+    // console.log(event.target.value)
+    const ufvalue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufvalue}/municipios`
+
+    fetch(url)
+    .then( res => res.json() )
+    .then( cidades => {
+
+        for (cidade of cidades){
+            citySelect.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`
+        }
+
+        citySelect.disabled = false
+        
+    })
+}
+
 document
     .querySelector("select[name=uf]")
-    .addEventListener("change", () => {
-        console.log("mudei")
-    } )
+    .addEventListener("change", getcities )
