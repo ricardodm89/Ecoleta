b98d7cb6c94d918b7908b63bffe7ee65bb8d9cc3
ricardodm89 <ricado89@gmail.com>
2020-06-13 15:26:53 -0300

    Configuração Index e rotas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const express = require("express")
 const server = express()
 
+// configurar pasta "public"
+server.use(express.static("public"))
+
+//configurar caminhos da aplicação
+//pagina inicial
+//req: requisição, pedido
+//res: resposta
+server.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", function(req, res){
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
 //iniciar servidor
 server.listen(3000)
