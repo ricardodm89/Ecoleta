ec940a9d4fbdb75b6be34b59ee7804c70c72e2b6
ricardodm89 <ricado89@gmail.com>
2020-06-15 14:01:59 -0300

    function INSERT TABLE - db.js

--- src/database/db.js
+++ src/database/db.js
@@ -9,8 +9,9 @@ db.serialize(() => {
         //criar uma tabela com comandos sql
         db.run(`
             CREATE TABLE IF NOT EXISTS places (
-                id INTEGER PRIMARY KEY AUTOINCREMENT
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 image TEXT,
+                name TEXT,
                 adress TEXT,
                 adress2 TEXT,
                 state TEXT,
@@ -20,6 +21,42 @@ db.serialize(() => {
         `)
 
         //inserir dados na tabela
+        const query = `
+            INSERT INTO places (
+                image,
+                name,
+                adress,
+                adress2,
+                state,
+                city,
+                items
+            ) VALUES (?,?,?,?,?,?,?);
+        `
+        const values = [
+            "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1401&q=80",
+            "Colectoria",
+            "Guilherme Gembala, Jardim América",
+            "N° 260",
+            "Santa Catarina",
+            "Rio do Sul",
+            "Resíduos Eletrônicos, Lâmpadas"
+        ]
+
+        function afterInsertData(err){
+            if (err) {
+                return console.log(err)
+            }
+
+            console.log("Cadastrado com sucesso")
+            console.log(this)
+        }
+
+        //db.run(query, values, afterInsertData)
+
+        
+        // consultar dados da tabela
+        
+
 
         //deletar um dado da tabela
 } )
